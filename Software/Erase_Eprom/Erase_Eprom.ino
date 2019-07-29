0be40af73b75ed266aa347ca522247e0995540bf
John P <WA2FZW@ARRL.net>
2019-07-29 09:44:18 -0400

    Add files via upload

---
+++ Software/Erase_Eprom/Erase_Eprom.ino
@@ -0,0 +1,21 @@
+#include <Wire.h> 					// Built-in
+#include <EEPROM.h>					// Built-in
+
+int 	epromSize = 1024;				// On the Mega
+int		epromAddress;					// EEPROM Address
+char	data = 0;						// What to put there
+
+void setup()
+{
+	Serial.begin ( 9600 );				// Initialize serial monitor
+	delay ( 1000 );
+
+	for ( epromAddress = 0; epromAddress < epromSize; epromAddress++ )
+		EEPROM.put ( epromAddress, data );
+
+	Serial.println ( "EEPROM erased" );
+
+}
+
+void loop() {}								// No loop!
+ 
---
+++ Software/Read_Eprom/Read_Eprom.ino
@@ -0,0 +1,30 @@
+#include <Wire.h> 					// Built-in
+#include <String.h>					// Built-in
+#include <EEPROM.h>					// Built-in
+
+int 			epromSize = 96;					// Max address used for analyzer
+int				epromAddress;					// EEPROM Address
+unsigned int	data = 0;						// What to put there
+
+void setup()
+{
+	Serial.begin ( 9600 );				// Initialize serial monitor
+	delay ( 1000 );
+
+	for ( epromAddress = 0; epromAddress < epromSize; epromAddress +=2 )
+	{
+		EEPROM.get ( epromAddress, data );
+
+		Serial.print ( epromAddress );
+		Serial.print ( "    " );
+
+		Serial.println ( data );
+
+	}
+
+	Serial.println ( "\n\nEEPROM finished" );
+
+}
+
+void loop() {}								// No loop!
+ 
